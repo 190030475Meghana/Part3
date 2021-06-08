d994fc6e0569c98a64b11719eedd369dabae74ed
190030475Meghana <74000513+190030475Meghana@users.noreply.github.com>
2021-06-08 10:13:06 +0530

    Update person.js

--- models/person.js
+++ models/person.js
@@ -8,8 +8,6 @@ const url = process.env.MONGODB_URI
 mongoose.connect(url, { useNewUrlParser: true })
   .then(() => console.log('Connected to DB'))
   .catch(error => console.error('DB connect error:', error.message))
-
-// Fix: DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead.
 mongoose.set('useCreateIndex', true)
 
 const personSchema = new mongoose.Schema({
